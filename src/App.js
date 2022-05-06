9d5d9ec1a1eeb3980bd85475f029948a281d7b80
Alex Koksharov <alex.kokshar23@gmail.com>
2022-05-06 20:32:30 +0200

    Add routing in project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/layouts/Header/Header';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Header />
+      <div className="content">
+        <main className="main">
+          <Outlet />
+        </main>
+        <footer>Foooter</footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
