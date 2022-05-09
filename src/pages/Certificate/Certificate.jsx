1385dce0bb609aace682beefe9c241e5f1dfbbe8
Alex Koksharov <alex.kokshar23@gmail.com>
2022-05-09 13:53:12 +0200

    Create Certificate description

--- src/pages/Certificate/Certificate.jsx
+++ src/pages/Certificate/Certificate.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Container from '../../components/UI/Container/Container';
 import CertificateProcess from '../../components/sections/SertificateProcess/SertificateProcess';
 import CertificateItem from '../../components/templates/CertificateItem/CertificateItem';
+import bag from '../../assets/images/bag.png';
 import './Certificate.scss';
 
 const data = [
@@ -57,6 +58,35 @@ function Certificate() {
           {data.map((item) => (
             <CertificateItem key={item.id} item={item} />
           ))}
+        </div>
+        <div className="certificate__description">
+          <div className="certificate__info">
+            <div className="certificate__text">
+              <h2 className="certificate__subtitle">
+                Самый лучший подарок, за которым никуда не нужно ходить
+              </h2>
+              <p className="certificate__paragraph reg">
+                Впервые в истории Level Kitchen! Подарочный Сертификат на полезное питание, который
+                можно оформить за считанные минуты.
+              </p>
+              <p className="certificate__paragraph reg">
+                Порадует друзей и близких, а также выручит в день рождения коллеги. Актуален на
+                Новый год и любые праздники. Или когда просто хочешь приятно удивить кого-то без
+                повода ;-)
+              </p>
+            </div>
+            <img src={bag} alt="Сумка" className="certificate__bag" />
+          </div>
+          <div className="certificate__period">
+            <div className="certificate__period-title">Действует целый год!</div>
+            <div className="certificate__period-text reg">
+              Сделай заказ за пару кликов, и уже через несколько минут красиво оформленный
+              Сертификат придёт к тебе на почту.
+            </div>
+            <div className="certificate__period-text reg">
+              Ты можешь его распечатать или оставить в электронном виде.
+            </div>
+          </div>
         </div>
 
         <CertificateProcess />
