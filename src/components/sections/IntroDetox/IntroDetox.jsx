ed43b618be3309ddb91c9f88c761307f7412e1aa
Alex Koksharov <alex.kokshar23@gmail.com>
2022-05-10 11:46:48 +0200

    Create IntroDetox section

---
+++ src/components/sections/IntroDetox/IntroDetox.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Container from '../../UI/Container/Container';
+import './IntroDetox.scss';
+
+function IntroDetox() {
+  return (
+    <div className="introdetox">
+      <Container>
+        <div className="introdetox__inner">
+          <div className="introdetox__content">
+            <h1 className="introdetox__title">Detox</h1>
+            <div className="introdetox__text">
+              Очищение и легкость. 8 бутылочек соков и смузи в день
+            </div>
+            <a href="#/" className="introdetox__link">
+              Попробовать
+            </a>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default IntroDetox;
--- src/pages/Detox/Detox.jsx
+++ src/pages/Detox/Detox.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { otherPrograms } from '../../data/OtherProgramsData';
 import Container from '../../components/UI/Container/Container';
+import IntroDetox from '../../components/sections/IntroDetox/IntroDetox';
 import OtherPrograms from '../../components/sections/OtherPrograms/OtherPrograms';
 import AboutCompany from '../../components/sections/AboutCompany/AboutCompany';
 import './Detox.scss';
@@ -10,6 +11,7 @@ function Detox() {
   let programs = [otherPrograms[0], ...otherPrograms.slice(2)];
   return (
     <div>
+      <IntroDetox />
       <Container>
         <OtherPrograms programs={programs} />
         <AboutCompany />
